b653a0595651dcaff4b948b12757d8fa441a2c10
rafiahmadfairuz <rafiahmadfairuz19@gmail.com>
2025-11-08 17:08:17 +0700

    vps

--- resources/views/components/jobfair/dashboard/app.blade.php
+++ resources/views/components/jobfair/dashboard/app.blade.php
@@ -10,6 +10,7 @@
         tailwind.config = {
             darkMode: 'class'
         }
+        
     </script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
 </head>
